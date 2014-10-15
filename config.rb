36f4cb8cf1b92c96a4cdb7be2c6c3e0bfa7847db
unknown <miguez@web09.princiti.com>
2014-10-15 10:23:05 -0300

    O config.rb não é mais necessário

--- config.rb
+++
@@ -1,8 +0,0 @@
-http_path = "/"
-css_dir = "./dist/css"
-sass_dir = "./assets/sass"
-images_dir = "./assets/img"
-javascripts_dir = "./assets/scripts"
-output_style = :compressed
-
-# output_style = :expanded or :nested or :compact or :compressed
